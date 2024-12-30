3c5e82e6ded2905084dfd764417293c67ea389e2
igor_frunze <igor_frunze@live.com>
2024-12-30 12:19:30 +0300

    initialization

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Div = styled.div`
+	text-align: center;
+`;
+
+function App() {
+	return (
+		<>
+			<Div>456</Div>
+		</>
+	);
+}
+
+export default App;
