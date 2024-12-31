140ee7d7aa9e6777742b6c0dc9710efd69647e13
igor_frunze <igor_frunze@live.com>
2024-12-31 13:35:21 +0300

    made the main commponent with routing

---
+++ src/Blog.jsx
@@ -0,0 +1,41 @@
+import { Route, Routes } from "react-router-dom";
+import styled from "styled-components";
+
+const Content = styled.div`
+	padding: 120px 0;
+`;
+
+const H2 = styled.h2`
+	text-align: center;
+`;
+
+const Header = () => {
+	return <h1>Шапка блога</h1>;
+};
+
+const Footer = () => {
+	return <h1>Подвал блога</h1>;
+};
+
+function Blog() {
+	return (
+		<>
+			<Header />
+			<Content>
+				<H2>Контент сраницы</H2>
+				<Routes>
+					<Route path="/" element={<div>Главная страница </div>} />
+					<Route path="/login" element={<div>Авторизация</div>} />
+					<Route path="/register" element={<div>Регистрация</div>} />
+					<Route path="/users" element={<div>Пользователи</div>} />
+					<Route path="/post" element={<div>Новая статья</div>} />
+					<Route path="/post/:postId" element={<div>Статья</div>} />
+					<Route path="*" element={<div>Ошибка</div>} />
+				</Routes>
+			</Content>
+			<Footer />
+		</>
+	);
+}
+
+export default Blog;
